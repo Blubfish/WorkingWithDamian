c4b84dc776f9af148ff99731131f62c7cfccda5f
moncheeta <moncheeta@duck.com>
2023-10-05 10:46:22 -0500

    Initial Commit

---
+++ WorkingWithDamian/WorkingWithDamianApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkingWithDamianApp.swift
+//  WorkingWithDamian
+//
+//  Created by Damian Myrda on 10/5/23.
+//
+
+import SwiftUI
+
+@main
+struct WorkingWithDamianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
